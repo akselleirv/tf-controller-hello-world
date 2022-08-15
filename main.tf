fd1ce6ca406b640fe9c1c798e3460735cf45f9ec
Aksel Skaar Leirvaag <akselsle@mobai.bio>
2022-08-15 15:39:53 +0200

    added null source

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+variable "subject" {
+   type = string
+   default = "World"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "Hello, ${var.subject}!"
+}
+
+resource "null_resource" "test" {
+}
